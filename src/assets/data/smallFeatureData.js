20b76a13c16a86b645e972cf6c73e1cdbf997ee3
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-05-04 12:00:47 +0600

    Updated

--- src/assets/data/smallFeatureData.js
+++ src/assets/data/smallFeatureData.js
@@ -1,20 +1,21 @@
+import natural from "@/assets/images/natural.png";
+import green from "@/assets/images/green.png";
+import safe from "@/assets/images/safe.png";
+
 export const smallFeatureData = [
   {
     name: "100% Natural",
     feature: "We use 100% natural ingredients in our products",
-    image:
-      "https://photohouseapi.photohousemagazine.com/uploads/1733663863494-image1.png",
+    image: natural,
   },
   {
     name: "Green Delivery",
     feature: "We use green delivery packaging system for all orders",
-    image:
-      "https://photohouseapi.photohousemagazine.com/uploads/1733663875986-image2.png",
+    image: green,
   },
   {
     name: "Safe Payment",
     feature: "Secure payment gateway for all payment methods",
-    image:
-      "https://photohouseapi.photohousemagazine.com/uploads/1733663893553-image4.png",
+    image: safe,
   },
 ];
