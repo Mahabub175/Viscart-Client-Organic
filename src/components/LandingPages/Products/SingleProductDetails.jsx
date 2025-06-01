f0b2bb49815b9f87fb1073f68020c7798f65187a
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-06-01 11:10:49 +0600

    Updated

--- src/components/LandingPages/Products/SingleProductDetails.jsx
+++ src/components/LandingPages/Products/SingleProductDetails.jsx
@@ -118,7 +118,9 @@ const SingleProductDetails = ({ params }) => {
       setCurrentVariant(updatedVariant);
 
       if (updatedVariant?.images) {
-        setVariantMedia(updatedVariant.images.map((image) => image));
+        setVariantMedia(
+          updatedVariant.images.map((image) => formatImagePath(image))
+        );
       } else {
         setVariantMedia([]);
       }
